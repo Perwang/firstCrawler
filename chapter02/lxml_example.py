239a7b9872220771b1f1c6bc374d0ffd78b1abdf
zhongruo <zhongruo@meili-inc.com>
2019-05-10 17:33:05 +0800

    添加了注释

--- chapter02/lxml_example.py
+++ chapter02/lxml_example.py
@@ -2,6 +2,19 @@ import lxml.html
 
 from urllib import request
 
+'''
+遇到的问题，安装完lxml以后，还是需要按照cssselect的
+'''
+'''
+选择所有标签：*
+选择<a>标签 a
+选择所有class="link"的元素：.link
+选择class="link"的<a>标签：a.link
+选择id="home"的<a>标签： a#home
+选择父元素为<a>标签的所有<span>子标签:a>span
+选择<a>标签内部的所有<span>标签 a span
+选择title属性为"home"的所有<a>标签：a[title=Home]
+'''
 def scrape(html):
     tree = lxml.html.fromstring(html)
     td = tree.cssselect('tr#places_area__row > td.w2p_fw')[0]# locate the area row
